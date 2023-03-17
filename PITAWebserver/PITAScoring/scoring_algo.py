Fixed graph scaling

PITAWebserver/PITAScoring/scoring_algo.py
@@ -158,9 +158,9 @@ def run(url1, url2):
     plt.title(f"{username}\n{exercise}")
     plt.xlabel("Frame")
     plt.ylabel("Score")
-    plt.savefig("output.jpg")
     plt.xlim(0, np.shape(user_body)[1])
     plt.ylim(0, 105)
+    plt.savefig("output.jpg")
     plt.show()
     return scoring(user_body, trainer_body)
 