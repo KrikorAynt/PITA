7fa71ef915fab19e00b78b9360c236d006fa400b
Alexan28 <71907704+Alexan28@users.noreply.github.com>
2023-04-09 21:48:11 -0400

    guiinstaller

--- GUIinstaller.py
+++ GUIinstaller.py
@@ -15,3 +15,16 @@ os.system("dotnet build")
 
 # Install vlc
 os.system("choco install vlc -y")
+
+import subprocess
+
+# Install chocolatey package manager
+subprocess.run(['powershell', '-Command', '(Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine)'])
+subprocess.run(['powershell', '-Command', r"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"])
+
+# Install Java using chocolatey
+subprocess.run(['choco', 'install', 'jdk8', '-y'])
+
+# Add Java to system path
+subprocess.run(['setx', 'PATH', r'%PATH%;C:\Program Files\Java\jdk1.8.0_281\bin'])
+
