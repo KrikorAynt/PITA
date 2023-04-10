ea43afe946723a1a146e64b10769d851a7dadaa8
Alexan28 <71907704+Alexan28@users.noreply.github.com>
2023-04-09 23:47:11 -0400

    UPDATE SIGNUP babyyyy

--- PITAGUI/src/main/SignUpGUI.java
+++ PITAGUI/src/main/SignUpGUI.java
@@ -1,15 +1,17 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,78 +20,125 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.net.URI;
 
 public class SignUpGUI {
+    private JTextField userText;
+    private JPasswordField passText;
+    protected JLabel infoLabel = new JLabel("", SwingConstants.CENTER);
+    private Image backgroundImage;
 
-	private JTextField userText;
-	private JPasswordField passText;
-	protected JLabel infoLabel = new JLabel("", SwingConstants.CENTER);
+    public SignUpGUI() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("PITA Sign Up");
+        frame.setPreferredSize(new Dimension(700, 400));
+
+        try {
+            backgroundImage = ImageIO.read(new File(".\\pita.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JPanel contentPane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(backgroundImage, 0, 0, this);
+            }
+        };
+        contentPane.setBackground(Color.ORANGE); // Set the background color to black
+
+        contentPane.setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel();
+        panel.setOpaque(false);
+        panel.setBorder(BorderFactory.createEmptyBorder(00, 400, 00, 10));
+        panel.setLayout(new GridLayout(0, 1));
+
+        JLabel label = new JLabel("SIGN UP HERE", SwingConstants.CENTER);
+        label.setForeground(Color.BLACK);
+        label.setFont(label.getFont().deriveFont(20f));
+
+        userText = new JTextField();
+        userText.setPreferredSize(new Dimension(100, 20));
+
+        passText = new JPasswordField();
+        passText.setPreferredSize(new Dimension(100, 20));
+
+        JButton button = new JButton("Sign Up");
+        // button.setPreferredSize(new Dimension(80, 30));
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String user = userText.getText();
+                @SuppressWarnings("deprecation")
+                String password = passText.getText();
+
+                if (create(user, password)) {
+                    new LoginGUI();
+                    user = "";
+                    password = "";
+                    userText.setText("");
+                    passText.setText("");
+                    frame.setVisible(false);
+                } else {
+                    user = "";
+                    password = "";
+                    userText.setText("");
+                    passText.setText("");
+                }
+            }
+        });
+
+        JButton login = new JButton("Login");
+        // login.setPreferredSize(new Dimension(80, 30));
+
+        login.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                new LoginGUI();
+                frame.setVisible(false);
+            }
+        });
+
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setOpaque(false);
+        fieldPanel.setLayout(new GridLayout(2, 2));
+        fieldPanel.add(new JLabel("Username:"));
+        fieldPanel.add(userText);
+        fieldPanel.add(new JLabel("Password:"));
+        fieldPanel.add(passText);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false);
+        buttonPanel.setLayout(new GridLayout(1, 2, 10, 10));
+        buttonPanel.add(button);
+        buttonPanel.add(login);
+
+        panel.add(label);
+        panel.add(fieldPanel);
+        panel.add(buttonPanel);
+        panel.add(infoLabel);
+
+        contentPane.add(panel, BorderLayout.CENTER);
+
+        frame.setContentPane(contentPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.getRootPane().setDefaultButton(button);
+        frame.setVisible(true);
+    }
+
+
 	
-	public SignUpGUI() {
-		JFrame frame = new JFrame();
-		
-		JButton signUp = new JButton("Sign Up");
-		signUp.addActionListener(new ActionListener() {
-
-	        @Override
-	        public void actionPerformed(ActionEvent event) {
-	        	String user = userText.getText();
-	    		@SuppressWarnings("deprecation")
-				String password = passText.getText();
-	    		
-	    		if(create(user, password)) {
-	    			new LoginGUI();
-	    			user = "";
-	    			password = "";
-	    			userText.setText("");
-	    			passText.setText("");
-	    			frame.setVisible(false);
-	    		}
-	    		else {
-	    			user = "";
-	    			password = "";
-	    			userText.setText("");
-	    			passText.setText("");
-	    		}	
-	        }
-	    });
-		
-		JButton login = new JButton("Login");
-		login.addActionListener(new ActionListener() {
-
-	        @Override
-	        public void actionPerformed(ActionEvent event) {
-	            new LoginGUI();
-	            frame.setVisible(false);
-	        }
-	    });
-		
-		JLabel label = new JLabel("SIGN UP HERE", SwingConstants.CENTER);
-		userText = new JTextField(20);
-		userText.setBounds(100, 20, 165, 25);
-		passText = new JPasswordField();
-		passText.setBounds(100, 20, 165, 25);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
-		panel.setLayout(new GridLayout(0,1));
-		panel.add(label);
-		panel.add(userText);
-		panel.add(passText);
-		panel.add(signUp);
-		panel.add(login);
-		panel.add(infoLabel);
-		
-		
-		frame.add(panel,BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("PETA Sign Up");
-		frame.getRootPane().setDefaultButton(signUp);
-		
-		
-		frame.pack();
-		frame.setVisible(true);
-	}
 	public boolean create(String username, String password) {
 		  
 		try {
